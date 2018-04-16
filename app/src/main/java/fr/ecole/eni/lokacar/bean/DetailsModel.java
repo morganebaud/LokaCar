ac025d916b46b6e85c1a4b461a4dd575e9a3deac
morganebaud <baud.morgane@gmail.com>
2018-04-16 14:30:00 +0200

    new package

--- app/src/main/java/fr/ecole/eni/lokacar/bean/DetailsModel.java
+++ app/src/main/java/fr/ecole/eni/lokacar/bean/DetailsModel.java
@@ -1,4 +1,186 @@
 package fr.ecole.eni.lokacar.bean;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 public class DetailsModel {
+
+    private int Id;
+    private double ConsommationExtraUrbaine;
+    private String BoiteDeVitesse;
+    private int MasseMini;
+    private int PuissanceAdministrative;
+    private int EmissionCo2;
+    private String Carrosserie;
+    private String Hybride;
+    private String Carburant;
+    private double ConsommationUrbaine;
+    private double ConsommationMixte;
+    private String CodeNationalIdentificationType;
+    private String DateMiseAjour;
+    private String Designation;
+
+    public DetailsModel() {
+    }
+
+    /**
+     *
+     * @param id
+     * @param consommationExtraUrbaine
+     * @param boiteDeVitesse
+     * @param masseMini
+     * @param puissanceAdministrative
+     * @param emissionCo2
+     * @param carrosserie
+     * @param hybride
+     * @param carburant
+     * @param consommationUrbaine
+     * @param consommationMixte
+     * @param codeNationalIdentificationType
+     * @param dateMiseAjour
+     * @param designation
+     */
+    public DetailsModel(int id,
+                        double consommationExtraUrbaine,
+                        String boiteDeVitesse,
+                        int masseMini,
+                        int puissanceAdministrative,
+                        int emissionCo2,
+                        String carrosserie,
+                        String hybride,
+                        String carburant,
+                        double consommationUrbaine,
+                        double consommationMixte,
+                        String codeNationalIdentificationType,
+                        String dateMiseAjour,
+                        String designation
+    ) {
+        Id = id;
+        ConsommationExtraUrbaine = consommationExtraUrbaine;
+        BoiteDeVitesse = boiteDeVitesse;
+        MasseMini = masseMini;
+        PuissanceAdministrative = puissanceAdministrative;
+        EmissionCo2 = emissionCo2;
+        Carrosserie = carrosserie;
+        Hybride = hybride;
+        Carburant = carburant;
+        ConsommationUrbaine = consommationUrbaine;
+        ConsommationMixte = consommationMixte;
+        CodeNationalIdentificationType = codeNationalIdentificationType;
+        DateMiseAjour = dateMiseAjour;
+        Designation = designation;
+    }
+
+    public String getDesignation() {
+        return Designation;
+    }
+
+    public void setDesignation(String designation) {
+        Designation = designation;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public double getConsommationExtraUrbaine() {
+        return ConsommationExtraUrbaine;
+    }
+
+    public void setConsommationExtraUrbaine(double consommationExtraUrbaine) {
+        ConsommationExtraUrbaine = consommationExtraUrbaine;
+    }
+
+    public String getBoiteDeVitesse() {
+        return BoiteDeVitesse;
+    }
+
+    public void setBoiteDeVitesse(String boiteDeVitesse) {
+        BoiteDeVitesse = boiteDeVitesse;
+    }
+
+    public int getMasseMini() {
+        return MasseMini;
+    }
+
+    public void setMasseMini(int masseMini) {
+        MasseMini = masseMini;
+    }
+
+    public int getPuissanceAdministrative() {
+        return PuissanceAdministrative;
+    }
+
+    public void setPuissanceAdministrative(int puissanceAdministrative) {
+        PuissanceAdministrative = puissanceAdministrative;
+    }
+
+    public int getEmissionCo2() {
+        return EmissionCo2;
+    }
+
+    public void setEmissionCo2(int emissionCo2) {
+        EmissionCo2 = emissionCo2;
+    }
+
+    public String getCarrosserie() {
+        return Carrosserie;
+    }
+
+    public void setCarrosserie(String carrosserie) {
+        Carrosserie = carrosserie;
+    }
+
+    public String getHybride() {
+        return Hybride;
+    }
+
+    public void setHybride(String hybride) {
+        Hybride = hybride;
+    }
+
+    public String getCarburant() {
+        return Carburant;
+    }
+
+    public void setCarburant(String carburant) {
+        Carburant = carburant;
+    }
+
+    public double getConsommationUrbaine() {
+        return ConsommationUrbaine;
+    }
+
+    public void setConsommationUrbaine(double consommationUrbaine) {
+        ConsommationUrbaine = consommationUrbaine;
+    }
+
+    public double getConsommationMixte() {
+        return ConsommationMixte;
+    }
+
+    public void setConsommationMixte(double consommationMixte) {
+        ConsommationMixte = consommationMixte;
+    }
+
+    public String getCodeNationalIdentificationType() {
+        return CodeNationalIdentificationType;
+    }
+
+    public void setCodeNationalIdentificationType(String codeNationalIdentificationType) {
+        CodeNationalIdentificationType = codeNationalIdentificationType;
+    }
+
+    public String getDateMiseAjour() {
+        return DateMiseAjour;
+    }
+
+    public void setDateMiseAjour(String dateMiseAjour) {
+        DateMiseAjour = dateMiseAjour;
+    }
+
 }
--- app/src/main/java/fr/ecole/eni/lokacar/bean/Model.java
+++ app/src/main/java/fr/ecole/eni/lokacar/bean/Model.java
@@ -1,4 +1,75 @@
 package fr.ecole.eni.lokacar.bean;
 
-public class Model {
+import android.provider.BaseColumns;
+
+public class Model implements BaseColumns {
+
+    private int Id;
+    private String Designation;
+    private String ModeleCommercial;
+    private String CNIT;
+    private String ModeleDossier;
+    private String Marque;
+
+    public Model(int id,
+                 String designation,
+                 String modeleCommercial,
+                 String CNIT,
+                 String modeleDossier,
+                 String marque) {
+        Id = id;
+        Designation = designation;
+        ModeleCommercial = modeleCommercial;
+        this.CNIT = CNIT;
+        ModeleDossier = modeleDossier;
+        Marque = marque;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getDesignation() {
+        return Designation;
+    }
+
+    public void setDesignation(String designation) {
+        Designation = designation;
+    }
+
+    public String getModeleCommercial() {
+        return ModeleCommercial;
+    }
+
+    public void setModeleCommercial(String modeleCommercial) {
+        ModeleCommercial = modeleCommercial;
+    }
+
+    public String getCNIT() {
+        return CNIT;
+    }
+
+    public void setCNIT(String CNIT) {
+        this.CNIT = CNIT;
+    }
+
+    public String getModeleDossier() {
+        return ModeleDossier;
+    }
+
+    public void setModeleDossier(String modeleDossier) {
+        ModeleDossier = modeleDossier;
+    }
+
+    public String getMarque() {
+        return Marque;
+    }
+
+    public void setMarque(String marque) {
+        Marque = marque;
+    }
 }
